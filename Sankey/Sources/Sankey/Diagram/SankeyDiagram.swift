f899c1b094d25236bbc827d0ffe90f50ce453137
Max Humber <max.humber@gmail.com>
2022-05-11 14:32:04 +0100

    flatten

--- Sankey/Sources/Sankey/Diagram/SankeyDiagram.swift
+++ Sankey/Sources/Sankey/Diagram/SankeyDiagram.swift
@@ -4,32 +4,61 @@ import WebKit
 public struct SankeyDiagram: UIViewRepresentable {
     @Binding public var links: [SankeyLink]
     public var options: SankeyOptions
-
+    
     public init(
         _ links: Binding<[SankeyLink]>,
         nodeColors: [String]? = nil,
+        nodeColorMode: SankeyOptions.Sankey.Node.ColorMode = .unique,
+        nodeWidth: Double? = nil,
+        nodePadding: Double? = nil,
         nodeLabelColor: String = "black",
         nodeLabelFontSize: Double = 30,
+        nodeLabelFontName: String? = nil,
+        nodeLabelBold: Bool = false,
+        nodeLabelItalic: Bool = false,
+        nodeLabelPadding: Double? = nil,
+        nodeInteractivity: Bool = false,
+        linkColors: [String]? = nil,
         linkColorMode: SankeyOptions.Sankey.Link.ColorMode? = nil,
+        linkColorFillOpacity: Double? = nil,
+        linkColorFill: String? = nil,
+        linkColorStroke: String? = nil,
+        linkColorStrokeWidth: Double = 0,
         tooltipValueLabel: String = "",
         tooltipTextColor: String = "black",
-        tooltipTextFontSize: Double = 30
+        tooltipTextFontSize: Double = 30,
+        tooltipTextFontName: String? = nil,
+        tooltipTextBold: Bool = false,
+        tooltipTextItalic: Bool = false,
+        layoutIterations: Int = 32
     ) {
         self._links = links
         self.options = .init(
             nodeColors: nodeColors,
+            nodeColorMode: nodeColorMode,
+            nodeWidth: nodeWidth,
+            nodePadding: nodePadding,
             nodeLabelColor: nodeLabelColor,
             nodeLabelFontSize: nodeLabelFontSize,
+            nodeLabelFontName: nodeLabelFontName,
+            nodeLabelBold: nodeLabelBold,
+            nodeLabelItalic: nodeLabelItalic,
+            nodeLabelPadding: nodeLabelPadding,
+            nodeInteractivity: nodeInteractivity,
+            linkColors: linkColors,
             linkColorMode: linkColorMode,
+            linkColorFillOpacity: linkColorFillOpacity,
+            linkColorFill: linkColorFill,
+            linkColorStroke: linkColorStroke,
+            linkColorStrokeWidth: linkColorStrokeWidth,
             tooltipValueLabel: tooltipValueLabel,
             tooltipTextColor: tooltipTextColor,
-            tooltipTextFontSize: tooltipTextFontSize
+            tooltipTextFontSize: tooltipTextFontSize,
+            tooltipTextFontName: tooltipTextFontName,
+            tooltipTextBold: tooltipTextBold,
+            tooltipTextItalic: tooltipTextItalic,
+            layoutIterations: layoutIterations
         )
-    }
-    
-    public init(_ links: Binding<[SankeyLink]>, options: SankeyOptions) {
-        self._links = links
-        self.options = options
     }
     
     public func makeUIView(context: Context) -> WKWebView {
@@ -68,5 +97,50 @@ public struct SankeyDiagram: UIViewRepresentable {
           </body>
         </html>
         """
+    }
+}
+
+struct SankeyDiagram_Previews: PreviewProvider {
+    static var previews: some View {
+        Preview()
+    }
+    
+    struct Preview: View {
+        @State var links: [SankeyLink] = [
+            ["A", "X", "5"],
+            ["A", "Y", "7"],
+            ["A", "Z", "6"],
+            ["B", "X", "2"],
+            ["B", "Y", "9"],
+            ["B", "Z", "4"]
+        ]
+        
+        var body: some View {
+            GeometryReader { geo in
+                VStack {
+                    addButton
+                    diagram(in: geo)
+                    Text("More Content")
+                }
+            }
+        }
+        
+        func diagram(in geo: GeometryProxy) -> some View {
+            SankeyDiagram(
+                $links,
+                nodeLabelFontSize: 40,
+                linkColorMode: .source,
+                tooltipTextFontSize: 40
+            )
+            .frame(height: geo.size.height * 0.5)
+        }
+    
+        var addButton: some View {
+            Button {
+                links.append(["C", "X", "5"])
+            } label: {
+                Text("Add")
+            }
+        }
     }
 }
