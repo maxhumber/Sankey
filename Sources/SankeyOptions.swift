96b715d6bf5478c3d79f2057baa308ec763a9fd0
Max Humber <max.humber@gmail.com>
2024-12-03 08:13:33 -0500

    docstring shorten

--- Sources/SankeyOptions.swift
+++ Sources/SankeyOptions.swift
@@ -1,7 +1,5 @@
 import Foundation
-import SwiftUI
 
-/// Configurable options for a Sankey diagram
 struct SankeyOptions {
     var nodeAlignment: SankeyNodeAlignment = .justify
     var nodeWidth: Double = 8
@@ -18,14 +16,14 @@ struct SankeyOptions {
     var labelFontFamily: String = "-apple-system, BlinkMacSystemFont, sans-serif"
 }
 
-/// Enumaration that specifies how link colors are determined in a Sankey Diagram
+/// Specifies how link colors are determined in a Sankey Diagram
 public enum SankeyLinkColorMode: String {
     case source
     case target
     case sourceTarget = "source-target"
 }
 
-/// Enumeration that specifies how nodes are aligned horizontally in a Sankey Diagram
+/// Specifies how nodes are aligned horizontally in a Sankey Diagram
 public enum SankeyNodeAlignment: String {
     case left = "Left"
     case right = "Right"
